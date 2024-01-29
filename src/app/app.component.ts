ae41f74e2b8143d5ea2e04b2a924f2c55dec7375
Jiri Nemec <jne@copsgmbh.com>
2024-01-29 20:19:05 +0100

    Basic application structure difnition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { MockDataService } from "./services/mock-data.service";
+import { Observable } from "rxjs";
+import { TableEntityModel } from "./model/table-entity.model";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'cemex-assignment';
+export class AppComponent implements OnInit{
+
+  public tableData$: Observable<TableEntityModel>;
+
+  constructor(private _mockDataService: MockDataService) {
+    this.tableData$ = this._mockDataService.getData();
+  }
+  ngOnInit(): void {
+  }
 }
---
+++ src/app/components/table-entity/table-entity.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-entity',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-entity.component.html',
+  styleUrl: './table-entity.component.scss'
+})
+export class TableEntityComponent {
+
+}
