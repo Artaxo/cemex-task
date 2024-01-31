123a2d62b8ece71ef679c35cc2fca30129869a75
Jiri Nemec <jne@copsgmbh.com>
2024-01-31 15:21:03 +0100

    app.component improvements and layout definition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { MockDataService } from "./services/mock-data.service";
 import { Observable } from "rxjs";
 import { TableEntityModel } from "./model/table-entity.model";
+import {TableComponent} from "./components/table/table.component";
+import {AsyncPipe} from "@angular/common";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [
+    TableComponent,
+    AsyncPipe
+  ],
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
 
-  public tableData$: Observable<TableEntityModel>;
+  public tableData$: Observable<TableEntityModel[]>;
 
   constructor(private _mockDataService: MockDataService) {
     this.tableData$ = this._mockDataService.getData();
